36926dad8189063f68b171211000f433e38c8e51
joeybutton <jbutton@middlebury.edu>
2016-05-08 22:32:44 -0400

    Try 2

--- src/app.js
+++ src/app.js
@@ -27,7 +27,11 @@ module.exports = function (testing) {
 		res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
 		// Request headers you wish to allow
-		// res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+		// Set to true if you need the website to include cookies in the requests sent
+		// to the API (e.g. in case you use sessions)
+		res.setHeader('Access-Control-Allow-Credentials', true);
 
 		// Pass to next layer of middleware
 		next();
